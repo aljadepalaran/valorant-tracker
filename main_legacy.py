fe65b4d1efb22116a6dd64073c99bbb701fc1142
Horep <horep245@gmail.com>
2022-08-10 06:09:33 +0100

    Updated the status box to actually change depending upon the error code

--- main_legacy.py
+++ main_legacy.py
@@ -5,24 +5,28 @@ from tkinter import ttk
 import numpy as np
 import PySimpleGUI as sg
 
+# this defines the window for tkinter
 root = Tk()
+# get the screen width and height of the user's window
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
+# window scaling factor compared to the users screen
 size_scale = .5
+# make window with the scaled size
 root.geometry(f"{int(screen_width * size_scale)}x{int(screen_height * size_scale)}")
 root.configure(background='grey')
 
 
 def draw_interface():  # create labels and entries
-    Label(root, text='Name').place(x=0, y=0, height=20, width=50)
+    Label(root, text='Name').place(x=0, y=0, height=20, width=50)  # name label and entry box
     name_entry = Entry(root, width=20)
     name_entry.place(x=75, y=0, height=20)
 
-    Label(root, text='Tag').place(x=0, y=30, height=20, width=50)
+    Label(root, text='Tag').place(x=0, y=30, height=20, width=50)  # tag label and entry box
     tag_entry = Entry(root, width=20)
     tag_entry.place(x=75, y=30, height=20)
 
-    Label(root, text='Status').place(x=0, y=60, height=20, width=50)
+    Label(root, text='Status').place(x=0, y=60, height=20, width=50)  # status label and entry box
     status_entry = Entry(root, width=20, state='disabled')
     status_entry.place(x=75, y=60, height=20)
 
@@ -30,11 +34,12 @@ def draw_interface():  # create labels and entries
         ttk.Label(root, text=f"test").pack()
         get_user_information(name_entry.get(), tag_entry.get())
 
-    def update_status(error_code):
-        status_entry.delete(0, END)
-        status_entry.insert(0, error_code)
+    def update_status(error_code):  # updates the box with the status error from getting the information
+        status_entry.configure(state='normal')
+        status_entry.insert('end', error_code)
+        status_entry.configure(state='disabled')
 
-    def get_user_information(_name, _tagline):
+    def get_user_information(_name, _tagline):  # requests the information from the website with appropriate name/tag
         try:
             request_url = f"https://api.henrikdev.xyz/valorant/v1/account/{_name}/{_tagline}"
             user_data = rq.get(request_url, timeout=1)
@@ -53,7 +58,7 @@ def draw_interface():  # create labels and entries
     ttk.Button(root, text="Load", command=button_command).pack()
 
 
-def clear_interface():
+def clear_interface():  # wipes the entire window
     for widget in root.winfo_children():
         widget.destroy()
 
@@ -65,8 +70,8 @@ def refresh_data():
     draw_interface()
 
 
-def parse_data(user_data):
-    count = 0
+def parse_data(user_data):  # reformats the received data and displays to the user
+    count = 0  # used to change the row in which the text is placed
     parsed_data = json.loads(user_data.content)
     for key in parsed_data['data']:
 
@@ -93,5 +98,5 @@ def parse_data(user_data):
         count += 1
 
 
-draw_interface()
+draw_interface()  # have to draw the interface for the first time
 root.mainloop()

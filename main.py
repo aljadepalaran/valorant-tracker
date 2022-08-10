64d3d0049e0b03253d491f59e2c0bdeeec35323a
Al Jade <aljade1902@gmail.com>
2022-08-10 06:00:44 +0100

    basic window

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Hello world!")
+import PySimpleGUI as sg
+
+
+def main():
+    layout = [[sg.Text("Name"), sg.Input(key='-NAME-')],
+              [sg.Text("Tag"), sg.Input(key='-TAG-')],
+              [sg.Button('Load', pad=(250,50))],
+              [sg.Text(size=(40, 1), key='-OUTPUT-')]]
+
+    window = sg.Window('Valorant Tracker', layout)
+
+    while True:
+        event, values = window.read()
+
+        if event == 'Load':
+            print('You clicked load.')
+            run_command()
+        elif event == sg.WINDOW_CLOSED:
+            break
+        window['-TAG-'].update('Name: ' + values['-NAME-'] + "! Thanks for trying PySimpleGUI")
+
+        # window['-TAG-'] refers to component with key='-TAG-'
+
+    window.close()
+
+
+def run_command():
+    print('Hello World!')
+
+
+main()

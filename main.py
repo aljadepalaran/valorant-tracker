c02f7680dc5f9c80d4075810af2ee8702022c0af
Al Jade <aljade1902@gmail.com>
2022-08-11 16:10:56 +0100

    compartmentalised layouts

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-from models.user import User
-from peewee import *
+import PySimpleGUI as sg
+from profile_view import profile_view
+from matches import matches_view
+
+layout_three = [[sg.Text('Hello world 3')]]
+
+tab_group = [
+    [
+        sg.TabGroup(
+            [
+                [
+                    sg.Tab('Profile View', profile_view(), background_color='green'),
+                    sg.Tab('Matches', matches_view(), background_color='blue'),
+                    sg.Tab('Layout 3', layout_three, background_color='pink'),
+                    sg.Button('Exit')
+                ]
+            ]
+        )
+    ]
+]
+
+window = sg.Window('Test', tab_group)
+
+while True:
+    event, values = window.read()
+    if event == 'Exit' or event == sg.WINDOW_CLOSED:
+        break
+    elif event == 'Load':
+        print('It works')
---
+++ matches.py
@@ -0,0 +1,5 @@
+import PySimpleGUI as sg
+
+
+def matches_view():
+    return [[sg.Text('These are my matches')]]

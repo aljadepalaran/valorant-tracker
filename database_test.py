02242481a06cd1be09bb6c6ebd7edfb62acb7957
Al Jade <aljade1902@gmail.com>
2022-08-11 15:06:43 +0100

    added try catch modified database access created seed data

--- database_test.py
+++ database_test.py
@@ -1,8 +1,18 @@
 from models.user import User
 from peewee import *
 
-User.create(puuid="test",
-            region="eu",
-            account_level=1,
-            name="c00k1e",
-            tag="1234")
+db = SqliteDatabase('main.db')  # define which db to use
+db.connect()  # connect to the db
+db.create_tables([User])  # create the database
+
+try:
+    User.create(puuid="SEED-1",
+                region="eu",
+                account_level=1,
+                image_small_url='SEED-image-small-1',
+                image_large_url='SEED-image-large-1',
+                image_wide_url='SEED-image-wide-1',
+                name="SEED-name-1",
+                tag="SEED-tag-1")
+except IntegrityError:
+    print('Something wrong')
--- models/user.py
+++ models/user.py
@@ -3,9 +3,9 @@ db = SqliteDatabase('main.db')
 
 
 class User(Model):
-    puuid = CharField()
+    puuid = CharField(unique=True)
     region = CharField()
-    account_level = CharField()
+    account_level = IntegerField()
     image_small_url = CharField()
     image_large_url = CharField()
     image_wide_url = CharField()

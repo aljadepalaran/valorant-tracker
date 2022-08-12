879c17f25a6da3c072430500e8279d7bbc3a5c02
Horep <horep245@gmail.com>
2022-08-12 08:46:46 +0100

    mild reformat
    Signed-off-by: Horep <horep245@gmail.com>

--- profile_view.py
+++ profile_view.py
@@ -2,9 +2,7 @@ import PySimpleGUI as sg
 
 
 def profile_view():
-    return [[sg.Text("Name"), sg.Input(key='-NAME-')],
-              [sg.Text("Tag"), sg.Input(key='-TAG-')],
-              [sg.Multiline(default_text="Awaiting input.", key='-MULTI-', size=(50, 10)),sg.Image(key='-SMALL-')],
+    return [[sg.Multiline(default_text="Awaiting input.", key='-MULTI-', size=(50, 10)),sg.Image(key='-SMALL-')],
               [sg.Image(key='-WIDE-')],
               [sg.Image(key='-LARGE-')],
               [sg.Button('Load', pad=(250, 50))],
